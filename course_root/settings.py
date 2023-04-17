62610bed73b8afa371ea6d7ecf9866da641dcece
Baiel Abdyllaev <abdyllaevbaiel.00@gmail.com>
2023-04-18 00:15:55 +0600

    added media src and dest to vercel.json

--- course_root/settings.py
+++ course_root/settings.py
@@ -144,7 +144,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_DIRS = os.path.join(BASE_DIR, 'media'),
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles_build', 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

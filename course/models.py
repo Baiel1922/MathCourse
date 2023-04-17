5809b83f0e34d9ccf23401cbb2420d63143026ed
Baiel Abdyllaev <abdyllaevbaiel.00@gmail.com>
2023-04-18 00:08:05 +0600

    added media src and dest to vercel.json

--- course/models.py
+++ course/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=250, unique=True, blank=False, null=False)
     text = models.TextField(blank=True, null=True)
-    cover = models.ImageField(upload_to='covers', blank=True, null=True)
+    cover = models.ImageField(upload_to='media', blank=True, null=True)
 
     def __str__(self):
         return self.title

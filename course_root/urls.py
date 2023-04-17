b05aea6c5842d0595c08da17fb81d877f465219a
Baiel Abdyllaev <abdyllaevbaiel.00@gmail.com>
2023-04-17 14:34:14 +0600

    added static settings

--- course_root/urls.py
+++ course_root/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
     # path('account/', include('account.urls')),
     # path('group/', include('group.urls')),
 
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
